fdbdd7da0ca1a1bea9182262b33ec3f3b201b258
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-02-26 14:25:54 -0800

    Create mouseOver

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const mainContainer = document.querySelector(".main-container");
 //Initialize grid
 for (let i = 0; i < 256; i++) {
     const square = document.createElement("div");
-    square.classList.add("square");              //Add class "square"
+    square.classList.add("square");              
     mainContainer.appendChild(square);
+
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "skyblue";
+    });
 }
