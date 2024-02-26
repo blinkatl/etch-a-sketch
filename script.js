8481e04558a015d188b05d340da333b215e04a92
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-02-26 15:49:34 -0800

    Create slider

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const mainContainer = document.querySelector(".main-container");
+const slider = document.querySelector(".slider");
+const sliderText = document.querySelector(".sliderText");
+const STARTING_VALUE_SLIDER = 16
+
+// Set the starting value of the slider
+slider.value = STARTING_VALUE_SLIDER;
 
 //Initialize grid
 for (let i = 0; i < 256; i++) {
@@ -10,3 +16,11 @@ for (let i = 0; i < 256; i++) {
         square.style.backgroundColor = "skyblue";
     });
 }
+
+//Get slider value
+slider.addEventListener("input",function() {
+    let sliderValue = this.value;
+    console.log(sliderValue);
+    sliderText.textContent = sliderValue + " x " + sliderValue;
+    sliderValue = 0;
+});
