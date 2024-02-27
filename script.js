4440d25357a5d9038c70b49cf19db7abafa769d3
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-02-26 22:02:49 -0800

    Create clearGrid and simplify code

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const mainContainer = document.querySelector(".main-container");
 const slider = document.querySelector(".slider");
 const sliderText = document.querySelector(".sliderText");
-const STARTING_VALUE_SLIDER = 16
+const STARTING_VALUE = 16;
 
-// Set the starting value of the slider
-slider.value = STARTING_VALUE_SLIDER;
-
-//Initialize grid
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");              
-    mainContainer.appendChild(square);
-
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "skyblue";
-    });
+//Remove previous squares
+function clearGrid() {
+    mainContainer.innerHTML = "";
 }
+
+//Create grid size based off slider value
+function createGrid(sliderValue) {
+    for (let i = 0; i < (sliderValue * sliderValue); i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = (600 / Math.sqrt(sliderValue**2)) + "px";
+        square.style.height = (600 / Math.sqrt(sliderValue**2)) + "px";                
+        mainContainer.appendChild(square);
+        
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "skyblue";
+        })
+    }
+}
+
+//Initialize grid and slider 
+createGrid(STARTING_VALUE);
+slider.value = STARTING_VALUE;
 
 //Get slider value
 slider.addEventListener("input",function() {
     let sliderValue = this.value;
-    console.log(sliderValue);
+    clearGrid();
+    createGrid(sliderValue);
+    
     sliderText.textContent = sliderValue + " x " + sliderValue;
     sliderValue = 0;
 });
